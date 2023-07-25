4ea700522ad45a946edfaada1fbd8e9f5cc0779a
vengateshm <40466166+vengateshm@users.noreply.github.com>
2023-07-25 16:43:37 +0530

    disposable effect lifecycle event observer

--- app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/compose_apis/ComposeApisActivity.kt
+++ app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/compose_apis/ComposeApisActivity.kt
@@ -5,21 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import dev.vengateshm.android_kotlin_compose_practice.compose_apis.remember_updated_state.Parent
-import dev.vengateshm.android_kotlin_compose_practice.compose_apis.remember_updated_state.Screen
+import androidx.compose.ui.platform.LocalLifecycleOwner
 
 class ComposeApisActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val lifecycleOwner = LocalLifecycleOwner.current
             MaterialTheme {
                 Surface(color = MaterialTheme.colors.background) {
 //                    Screen()
 //                    Parent()
 //                    ProduceStateSample()
 //                    ProduceStateSampleLive()
-                    DisposableStateScreen()
+                    //DisposableStateScreen()
+                    PerformOnLifeCycle(
+                        lifecycleOwner = lifecycleOwner,
+                        onStart = {
+                            println("onStart Event")
+                        },
+                        onResume = {
+                            println("onResume Event")
+                        })
                 }
             }
         }
--- app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/compose_apis/DisposableStateSample.kt
+++ app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/compose_apis/DisposableStateSample.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.wifi.WifiManager
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
@@ -19,6 +20,9 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 
 @Composable
 fun DisposableStateScreen() {
@@ -85,6 +89,27 @@ fun SystemBroadcastReceiver(
         context.registerReceiver(receiver, intentFilter)
         onDispose {
             context.unregisterReceiver(receiver)
+        }
+    }
+}
+
+@Composable
+fun PerformOnLifeCycle(
+    lifecycleOwner: LifecycleOwner,
+    onStart: () -> Unit = {},
+    onResume: () -> Unit = {},
+) {
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_START -> onStart()
+                Lifecycle.Event.ON_RESUME -> onResume()
+                else -> Log.i("OBSERVER", "Lifecycle: ${event.name}")
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
         }
     }
 }
