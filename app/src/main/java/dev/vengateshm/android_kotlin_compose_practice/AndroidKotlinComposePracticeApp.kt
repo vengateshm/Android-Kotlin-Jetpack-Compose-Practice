13809304dee2691e4eabc40be880cc1611dab80b
vengateshm <40466166+vengateshm@users.noreply.github.com>
2023-11-16 10:56:41 +0530

    handle exception globally in application class

--- app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/AndroidKotlinComposePracticeApp.kt
+++ app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/AndroidKotlinComposePracticeApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import coil.ImageLoader
 import coil.ImageLoaderFactory
@@ -12,6 +13,11 @@ import coil.memory.MemoryCache
 import coil.request.CachePolicy
 import coil.util.DebugLogger
 import dagger.hilt.android.HiltAndroidApp
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.lang.Character.LINE_SEPARATOR
+import kotlin.system.exitProcess
+
 
 @HiltAndroidApp
 class AndroidKotlinComposePracticeApp : Application(), ImageLoaderFactory {
@@ -28,6 +34,60 @@ class AndroidKotlinComposePracticeApp : Application(), ImageLoaderFactory {
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            handleUncaughtException(throwable)
+        }
+    }
+
+    private fun handleUncaughtException(throwable: Throwable) {
+        val intent = Intent(this.applicationContext, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+
+        println(getErrorString(throwable))
+
+        android.os.Process.killProcess(android.os.Process.myPid())
+        // Exit the current process
+        // Non zero status - Abnormal termination
+        exitProcess(10)
+    }
+
+    private fun getErrorString(throwable: Throwable): String {
+        val stackTrace = StringWriter()
+        throwable.printStackTrace(PrintWriter(stackTrace))
+        val errorReport = StringBuilder()
+        errorReport.append("************ CAUSE OF ERROR ************\n\n")
+        errorReport.append(stackTrace.toString())
+
+        errorReport.append("\n************ DEVICE INFORMATION ***********\n")
+        errorReport.append("Brand: ")
+        errorReport.append(Build.BRAND)
+        errorReport.append(LINE_SEPARATOR)
+        errorReport.append("Device: ")
+        errorReport.append(Build.DEVICE)
+        errorReport.append(LINE_SEPARATOR)
+        errorReport.append("Model: ")
+        errorReport.append(Build.MODEL)
+        errorReport.append(LINE_SEPARATOR)
+        errorReport.append("Id: ")
+        errorReport.append(Build.ID)
+        errorReport.append(LINE_SEPARATOR)
+        errorReport.append("Product: ")
+        errorReport.append(Build.PRODUCT)
+        errorReport.append(LINE_SEPARATOR)
+        errorReport.append("\n************ FIRMWARE ************\n")
+        errorReport.append("SDK: ")
+        errorReport.append(Build.VERSION.RELEASE)
+        errorReport.append(LINE_SEPARATOR)
+        errorReport.append("Release: ")
+        errorReport.append(Build.VERSION.RELEASE)
+        errorReport.append(LINE_SEPARATOR)
+        errorReport.append("Incremental: ")
+        errorReport.append(Build.VERSION.INCREMENTAL)
+        errorReport.append(LINE_SEPARATOR)
+
+        return errorReport.toString()
     }
 
     override fun newImageLoader(): ImageLoader {
