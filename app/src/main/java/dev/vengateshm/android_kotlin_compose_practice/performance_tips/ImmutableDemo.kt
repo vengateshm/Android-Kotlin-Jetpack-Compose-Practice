01431401466337715c69a2ff70c6ff7acf2be48f
vengateshm <vengateshm.13@gmail.com>
2023-12-27 12:39:13 +0530

    avoid unnecessary recomposition using lambdas

--- app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/performance_tips/ImmutableDemo.kt
+++ app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/performance_tips/ImmutableDemo.kt
@@ -15,9 +15,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
-
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 @Composable
 fun ContactListScreen() {
@@ -26,13 +29,45 @@ fun ContactListScreen() {
         Checkbox(checked = selected, onCheckedChange = {
             selected = it
         })
-        //ContactList(state = ContactListState(false, listOf("Person")))
-        ImmutableContactList(isLoading = false, names = persistentListOf("Person"))
+//        ContactList(isLoading = false, names = listOf("Person"))
+//        ContactList(state = ContactListState(false, listOf("Person")))
+//        ImmutableContactList(isLoading = false, names = persistentListOf("Person"))
+    }
+}
+
+@Composable
+fun ContactListScreen(viewModel: ContactListViewModel) {
+    val isSelected = viewModel.isSelected.collectAsStateWithLifecycle()
+    val state = viewModel.state.collectAsStateWithLifecycle()
+    Column {
+        Checkbox(checked = isSelected.value, onCheckedChange = /*{
+            viewModel.checkboxState(it)
+        }*/viewModel::checkboxState)
+        ContactList(
+            isLoading = { state.value.isLoading },
+            names = { state.value.names }
+        )
     }
 }
 
 @Composable
 fun ContactList(
+    isLoading: () -> Boolean,
+    names: () -> List<String>
+) {
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        if (isLoading()) {
+            CircularProgressIndicator()
+        } else {
+            Text(text = names().toString())
+        }
+    }
+}
+
+@Composable
+fun ContactListWithState(
     state: ContactListState
 ) {
     Box(
@@ -68,3 +103,26 @@ data class ContactListState(
     val isLoading: Boolean,
     val names: List<String>
 )
+
+data class ContactListUiState(
+    val isLoading: Boolean,
+    val names: List<String>
+)
+
+class ContactListViewModel : ViewModel() {
+    private val _isSelected = MutableStateFlow(false)
+    val isSelected = _isSelected.asStateFlow()
+
+    private val _state = MutableStateFlow(
+        ContactListUiState(
+            isLoading = false,
+            names = persistentListOf("Person")
+        )
+    )
+
+    val state = _state.asStateFlow()
+
+    fun checkboxState(value: Boolean) {
+        _isSelected.value = value
+    }
+}
--- app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/performance_tips/PerformanceTipsActivity.kt
+++ app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/performance_tips/PerformanceTipsActivity.kt
@@ -3,9 +3,12 @@ package dev.vengateshm.android_kotlin_compose_practice.performance_tips
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 
 class PerformanceTipsActivity : ComponentActivity() {
+
+    val viewModel:ContactListViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,7 +24,9 @@ class PerformanceTipsActivity : ComponentActivity() {
                 // Avoid backwards writing
 //                PerfTip4()
                 // Wait until you read state as long as possible
-                PerfTip5()
+//                PerfTip5()
+                // Immutable
+                ContactListScreen(viewModel)
             }
         }
     }
