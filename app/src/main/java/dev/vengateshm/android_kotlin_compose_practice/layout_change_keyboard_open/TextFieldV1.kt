6eb9ae44beeab873639485fa7499a06e7640764f
vengateshm <vengateshm.13@gmail.com>
2024-01-09 14:23:40 +0530

    hide composables based on keyboard visibility - login screen usecase

---
+++ app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/layout_change_keyboard_open/TextFieldV1.kt
@@ -0,0 +1,77 @@
+package dev.vengateshm.android_kotlin_compose_practice.layout_change_keyboard_open
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TextFieldV1(
+    modifier: Modifier = Modifier,
+    label: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions,
+    keyboardActions: KeyboardActions,
+    readOnly: Boolean = false,
+    height: Dp = 40.dp,
+    trailingIcon: ImageVector? = null
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(text = label)
+        Spacer(modifier = Modifier.height(8.dp))
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            readOnly = readOnly
+        ) {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(10.dp))
+                    .height(height)
+                    .background(color = Color(0XFFEFEEEE)),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier.weight(1f)
+                        .padding(horizontal = 10.dp),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    it.invoke()
+                }
+                trailingIcon?.let {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = it, contentDescription = null,
+                            tint = Color(0XFF828282)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
