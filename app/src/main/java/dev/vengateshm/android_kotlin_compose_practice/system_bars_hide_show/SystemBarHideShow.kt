893fdb69dadecbd8e0b5a1bf8395a08bb0e73729
vengateshm <vengateshm.13@gmail.com>
2024-01-22 23:37:14 +0530

    show or hide system bars

---
+++ app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/system_bars_hide_show/SystemBarHideShow.kt
@@ -0,0 +1,55 @@
+package dev.vengateshm.android_kotlin_compose_practice.system_bars_hide_show
+
+import android.app.Activity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+@Composable
+fun SystemBarHideShow() {
+    var showSystemBar by remember {
+        mutableStateOf(true)
+    }
+
+    val view = LocalView.current
+    val window = (view.context as Activity).window
+    val insetsController = WindowCompat.getInsetsController(window, view)
+    if (!view.isInEditMode) {
+        if (showSystemBar) {
+            insetsController.apply {
+                show(WindowInsetsCompat.Type.systemBars())
+            }
+        } else {
+            insetsController.apply {
+                hide(WindowInsetsCompat.Type.systemBars())
+                systemBarsBehavior =
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = {
+            showSystemBar = !showSystemBar
+        }) {
+            Text(text = "Hide/Show System Bars")
+        }
+    }
+}
