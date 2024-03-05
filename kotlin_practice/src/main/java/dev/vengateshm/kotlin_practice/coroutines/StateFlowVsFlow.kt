5aa5787af1eb6db90820af8b7a22d6e3c3952693
vengateshm <vengateshm.13@gmail.com>
2024-03-05 23:09:32 +0530

    state flow, flow behaviour description

--- kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/coroutines/StateFlowVsFlow.kt
+++ kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/coroutines/StateFlowVsFlow.kt
@@ -1,9 +1,12 @@
 package dev.vengateshm.kotlin_practice.coroutines
 
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -37,4 +40,32 @@ suspend fun collectFlow(flow: Flow<String>) {
         println("Collecting flow")
     }
     println("After flow")
+}
+
+/**
+ * No, a StateFlow does not complete in the same way as a Flow.
+ * In a Flow, you can have a complete signal, which means that the Flow has completed and will not emit any more items. This is typically represented by the onCompletion operator or a terminal operator like collect with a defined termination condition.
+ * In contrast, a StateFlow is designed to represent a continuous and reactive state, such as the current state of a UI component. It does not have a concept of completion because it's meant to always reflect the current state and automatically update whenever the underlying value changes.
+ * A StateFlow doesn't complete because it's expected to be observed continuously as long as the associated object or state exists. It will emit new values whenever the value it represents changes, making it suitable for observing changes in state over time.
+ * So, in summary, while a Flow can complete and signal the end of emission, a StateFlow is designed to always be available for observation and reflect the current state.
+ * */
+
+class MainViewModel {
+    val flow = flow { emit(1) }
+    val stateFlow = MutableStateFlow(1)
+
+    init {
+        GlobalScope.launch {
+            stateFlow.collectLatest {
+
+            }
+            println("StateFlow")
+        }
+        GlobalScope.launch {
+            flow.collectLatest {
+
+            }
+            println("Flow")
+        }
+    }
 }
