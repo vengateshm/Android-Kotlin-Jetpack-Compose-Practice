483bfb42d6a6c7aad8d9690dc963826ad39cb824
vengateshm <vengateshm.13@gmail.com>
2024-03-05 23:33:39 +0530

    valid method overloading scenarios

---
+++ kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/std_library_samples/ValidMethodOverloadingScenarios.kt
@@ -0,0 +1,41 @@
+package dev.vengateshm.kotlin_practice.std_library_samples
+
+// Type should be different
+fun add(a: Int, b: Int) {
+
+}
+
+fun add(a: Double, b: Double) {
+
+}
+
+// Number of parameter must be different
+
+fun add1(a: Int, b: Int) {
+
+}
+
+fun add1(a: Int, b: Int, c: Int) {
+
+}
+
+// Order of the parameters must be different
+
+fun find(name: String, id: Int) {
+
+}
+
+fun find(id: Int, name: String) {
+
+}
+
+// Throws error when return type is different but method signature is same
+
+/*
+fun add2(a:Int, b:Int):Int {
+
+}
+
+fun add2(a:Int, b:Int):Double {
+
+}*/
