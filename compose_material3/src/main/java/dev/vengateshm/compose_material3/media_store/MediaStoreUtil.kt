71ee4408dc5dea12127d758f23d0d575df2585c2
vengateshm <vengateshm.13@gmail.com>
2024-04-26 18:02:48 +0530

    media store api save image

---
+++ compose_material3/src/main/java/dev/vengateshm/compose_material3/media_store/MediaStoreUtil.kt
@@ -0,0 +1,55 @@
+package dev.vengateshm.compose_material3.media_store
+
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.annotation.RequiresApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+@RequiresApi(Build.VERSION_CODES.Q)
+class MediaStoreUtil(private val context: Context) {
+    suspend fun saveImage(bitmap: Bitmap) {
+        withContext(Dispatchers.IO) {
+            val imageCollection = MediaStore.Images.Media.getContentUri(
+                MediaStore.VOLUME_EXTERNAL_PRIMARY
+            )
+            val timeInMillis = System.currentTimeMillis()
+
+            val imageContentValues = ContentValues().apply {
+                put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                put(MediaStore.Images.Media.DISPLAY_NAME, "image-${timeInMillis}.jpg")
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+                put(MediaStore.Images.Media.DATE_TAKEN, timeInMillis)
+                put(MediaStore.Images.Media.IS_PENDING, 1)
+            }
+
+            val resolver = context.contentResolver
+            val imageMediaStoreUri = resolver.insert(
+                imageCollection, imageContentValues
+            )
+
+            imageMediaStoreUri?.let { uri ->
+                try {
+                    resolver.openOutputStream(uri)?.let { outputStream ->
+                        bitmap.compress(
+                            Bitmap.CompressFormat.JPEG, 100, outputStream
+                        )
+                    }
+                    imageContentValues.clear()
+                    imageContentValues.put(MediaStore.Images.Media.IS_PENDING, 0)
+
+                    resolver.update(
+                        uri, imageContentValues, null, null
+                    )
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    resolver.delete(uri, null, null)
+                }
+            }
+        }
+    }
+}
