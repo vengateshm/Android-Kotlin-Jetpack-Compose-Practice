e097c0cfd54546bf5c225c492ad99a9b69646acf
vengateshm <vengateshm.13@gmail.com>
2024-05-06 21:49:32 +0530

    required width modifier - occupies space required discarding the parent size constraints

---
+++ compose_material3/src/main/java/dev/vengateshm/compose_material3/layout_apis/Element.kt
@@ -0,0 +1,23 @@
+package dev.vengateshm.compose_material3.layout_apis
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Element(modifier: Modifier = Modifier) {
+    Spacer(modifier = Modifier.height(4.dp))
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(16.dp)
+            .background(color = Color.Gray),
+    )
+    Spacer(modifier = Modifier.height(4.dp))
+}
---
+++ compose_material3/src/main/java/dev/vengateshm/compose_material3/layout_apis/RequiredSizeSample.kt
@@ -0,0 +1,26 @@
+package dev.vengateshm.compose_material3.layout_apis
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RequiredSizeSample(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.LightGray)
+            .padding(16.dp)
+    ) {
+        Element()
+        Element(modifier = Modifier.requiredWidth(1000.dp))
+        Element()
+        Element()
+    }
+}
