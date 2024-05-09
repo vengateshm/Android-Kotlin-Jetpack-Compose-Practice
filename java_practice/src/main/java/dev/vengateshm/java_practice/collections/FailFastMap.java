a4917bd9c95d6cbd4e6dd06da4153a757299f106
vengateshm <vengateshm.13@gmail.com>
2024-05-09 20:07:02 +0530

    fail fast map prevention using concurrent hash map

---
+++ java_practice/src/main/java/dev/vengateshm/java_practice/collections/FailFastMap.java
@@ -0,0 +1,24 @@
+package dev.vengateshm.java_practice.collections;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class FailFastMap {
+
+    public static void main(String[] args) {
+        //Map<Integer, String> map = new HashMap<>();
+        Map<Integer, String> map = new ConcurrentHashMap<>();
+        map.put(1, "one");
+        map.put(2, "two");
+
+        Iterator it = map.keySet().iterator();
+
+        while (it.hasNext()) {
+            Integer key = (Integer) it.next();
+            System.out.println(key + " : " + map.get(key));
+            map.put(3, "three");
+        }
+    }
+}
