4446723a942db7b149ffe06f138dd8e1d2aef274
vengateshm <vengateshm.13@gmail.com>
2024-05-12 18:32:04 +0530

    ring and piechart using canvas

---
+++ compose_material3/src/main/java/dev/vengateshm/compose_material3/custom_ui/charts/PieChartSample.kt
@@ -0,0 +1,111 @@
+package dev.vengateshm.compose_material3.custom_ui.charts
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlin.random.Random
+
+data class Score(
+    val subject: String,
+    val score: Int
+)
+
+data class PieChartData(
+    val title: String,
+    val point: Int,
+    val color: Color = Color.random()
+)
+
+private fun Color.Companion.random(): Color {
+    return Color(Random.nextLong(0XFFFFFFFF))
+}
+
+@Composable
+fun PieChartSample(modifier: Modifier = Modifier) {
+    val scores = listOf(
+        Score(subject = "English", score = 80),
+        Score(subject = "Science", score = 90),
+        Score(subject = "Mathematics", score = 97),
+        Score(subject = "Computer Science", score = 100),
+        Score(subject = "Economics", score = 53),
+    )
+    PieChart(modifier = modifier, chartDataPoints = scores.map {
+        PieChartData(
+            title = it.subject,
+            point = it.score,
+        )
+    })
+}
+
+@Composable
+fun PieChart(modifier: Modifier = Modifier, chartDataPoints: List<PieChartData>) {
+    val total = chartDataPoints.sumOf { it.point }
+    Canvas(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        var startAngle = 270f
+        chartDataPoints.forEachIndexed { index, pieChartData ->
+            val sweepAngle = 360f * (pieChartData.point.toFloat() / total)
+            drawPie(
+                color = pieChartData.color,
+                startAngle = startAngle,
+                sweepAngle = sweepAngle,
+            )
+            startAngle += sweepAngle
+        }
+    }
+}
+
+fun DrawScope.drawRing(
+    color: Color,
+    startAngle: Float,
+    sweepAngle: Float
+) {
+    val padding = 100f
+    this.drawArc(
+        color = color,
+        startAngle = startAngle,
+        sweepAngle = sweepAngle,
+        useCenter = false,
+        size = Size(width = size.width - padding, height = size.height - padding),
+        style = Stroke(width = 20f, cap = StrokeCap.Butt),
+        topLeft = Offset(x = padding / 2f, y = padding / 2f)
+    )
+}
+
+fun DrawScope.drawPie(
+    color: Color,
+    startAngle: Float,
+    sweepAngle: Float
+) {
+    val padding = 100f
+    this.drawArc(
+        color = color,
+        startAngle = startAngle,
+        sweepAngle = sweepAngle,
+        useCenter = true,
+        size = Size(width = size.width - padding, height = size.height - padding),
+        topLeft = Offset(x = padding / 2f, y = padding / 2f)
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun PieChartSamplePreview() {
+    PieChartSample(
+        modifier = Modifier.size(200.dp)
+    )
+}
