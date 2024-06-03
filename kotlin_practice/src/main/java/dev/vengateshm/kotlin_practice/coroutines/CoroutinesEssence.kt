fa3cf3a2e49f0f0cc02247214de45438a35cbdf6
vengateshm <vengateshm.13@gmail.com>
2024-06-03 18:11:44 +0530

    coroutines low level apis

---
+++ kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/coroutines/CoroutinesEssence.kt
@@ -0,0 +1,29 @@
+package dev.vengateshm.kotlin_practice.coroutines
+
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.intrinsics.COROUTINE_SUSPENDED
+import kotlin.coroutines.intrinsics.createCoroutineUnintercepted
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+fun main() {
+    var cow = 0
+    println("Milking cow ${++cow}"); feedChickens.resume(Unit)
+    println("Milking cow ${++cow}"); feedChickens.resume(Unit)
+    println("Milking cow ${++cow}"); feedChickens.resume(Unit)
+    println("Milking cow ${++cow}"); feedChickens.resume(Unit)
+}
+
+val feedChickens = suspend {
+    var chicken = 0
+    println("Feeding chicken #${++chicken}")
+    // Suspension point
+    suspendCoroutine<Unit> { COROUTINE_SUSPENDED }
+    println("Feeding chicken #${++chicken}")
+    suspendCoroutine<Unit> { COROUTINE_SUSPENDED }
+    println("Feeding chicken #${++chicken}")
+    suspendCoroutine<Unit> { COROUTINE_SUSPENDED }
+    println("Feeding chicken #${++chicken}")
+    suspendCoroutine<Unit> { COROUTINE_SUSPENDED }
+}.createCoroutineUnintercepted(Continuation(EmptyCoroutineContext) {})
