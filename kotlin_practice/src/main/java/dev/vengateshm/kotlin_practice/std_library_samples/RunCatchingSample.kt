0a2e84a5f3a4a0e04d3bf10461406fad6605cbf5
vengateshm <vengateshm.13@gmail.com>
2024-07-01 18:10:43 +0530

    runSafeSuspendCatching function

--- kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/std_library_samples/RunCatchingSample.kt
+++ kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/std_library_samples/RunCatchingSample.kt
@@ -1,15 +1,50 @@
 package dev.vengateshm.kotlin_practice.std_library_samples
 
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 fun main() {
     val result = runCatching {
-        completeSuccess(400)
+        completeSuccess(200)
     }
     println(result.getOrNull())
     println(result.exceptionOrNull())
+
+    result
+        .onSuccess {
+            println(it)
+        }
+        .onFailure {
+            println(it)
+        }
+
+    runBlocking {
+        val job = launch {
+            runSafeSuspendCatching {
+                delay(5000L)
+                "Hello"
+            }
+        }
+        delay(1000)
+        job.cancel()
+    }
 }
 
 fun completeSuccess(code: Int): String {
     if (code != 200)
         throw RuntimeException("Error!")
     return "Success!"
+}
+
+inline suspend fun <R> runSafeSuspendCatching(block: () -> R): Result<R> {
+    return try {
+        Result.success(block())
+    } catch (ce: CancellationException) {
+        println(ce)
+        throw ce
+    } catch (e: Throwable) {
+        Result.failure(e)
+    }
 }
