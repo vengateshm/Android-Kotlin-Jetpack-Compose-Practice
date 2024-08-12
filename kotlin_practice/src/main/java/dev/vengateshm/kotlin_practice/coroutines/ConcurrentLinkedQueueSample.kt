117b1f0fd7ccecd9edc3a2b2ab9a2686b09ed531
vengateshm <vengateshm.13@gmail.com>
2024-08-12 22:01:50 +0530

    concurrent linked queue sample

---
+++ kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/coroutines/ConcurrentLinkedQueueSample.kt
@@ -0,0 +1,27 @@
+package dev.vengateshm.kotlin_practice.coroutines
+
+import java.lang.Thread.sleep
+import java.util.concurrent.ConcurrentLinkedQueue
+import kotlin.concurrent.thread
+import kotlin.random.Random
+
+val queue = ConcurrentLinkedQueue<String>()
+
+fun main() {
+
+    thread {
+
+        repeat(5) { sleep(1000); queue.add(readData(1)) }
+    }
+    thread {
+        repeat(5) { queue.add(readData(2)) }
+    }
+
+    while (true) {
+        val data = queue.poll() ?: continue
+        processData(data)
+    }
+}
+
+fun readData(sourceId: Int) = "Source::$sourceId::${Random.nextInt(1000)}"
+fun processData(data: String) = println(data)
