f7f1db9e0926accd9d4226879353a5cf9f57a169
vengateshm <vengateshm.13@gmail.com>
2024-08-21 13:28:41 +0530

    composable api design prefixing, composition locals, component defaults for styling

--- compose_material3/src/main/java/dev/vengateshm/compose_material3/api_compose/ComposableApiDesignSample.kt
+++ compose_material3/src/main/java/dev/vengateshm/compose_material3/api_compose/ComposableApiDesignSample.kt
@@ -1,16 +1,31 @@
 package dev.vengateshm.compose_material3.api_compose
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ComposableApiDesignSample(
     requiredParam: String, // Required
-    modifier: Modifier = Modifier, // Optional
+    modifier: Modifier = Modifier, // Modifier
+    icon: @Composable (() -> Unit)? = null, // Default Nullable
+    text: @Composable () -> Unit = {}, // Default Empty
+    enabled: Boolean = true, // Optional
+    color: Color = Color.LightGray, // Short inline styles
+    shape: Shape = ComponentDefaults.componentShape(), // ComponentDefaults for long list of styles
     content: @Composable () -> Unit, // Trailing content lambda
 ) {
 
+}
+
+object ComponentDefaults {
+    @Composable
+    fun componentShape() = RoundedCornerShape(16.dp)
 }
 
 // Return UI
@@ -19,7 +34,7 @@ fun Header(modifier: Modifier = Modifier) {
 
 }
 
-// Return state
+// Return state with remember
 class HeaderData
 
 @Composable
@@ -28,3 +43,22 @@ fun rememberHeaderState(): HeaderData {
         HeaderData()
     }
 }
+
+// Prefixing
+@Composable
+fun Chip(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun BasicChip(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun FilterChip(modifier: Modifier = Modifier) {
+
+}
+
+// Composition Locals
+val LocalAppTheme = compositionLocalOf { }
