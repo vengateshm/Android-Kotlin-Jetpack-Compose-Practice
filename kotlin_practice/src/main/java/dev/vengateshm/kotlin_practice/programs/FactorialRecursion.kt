dfd2fe258ae5bb9b130b9e1ba63791d10d2c25dc
vengateshm <vengateshm.13@gmail.com>
2024-11-04 22:07:23 +0530

    factorial tailrec (imperative), DeepRecursiveFunction

--- kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/programs/FactorialRecursion.kt
+++ kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/programs/FactorialRecursion.kt
@@ -1,5 +1,7 @@
 package dev.vengateshm.kotlin_practice.programs
 
+import java.math.BigInteger
+
 fun main() {
     factorialRecursion(0).also(::println)
     factorialRecursion(1).also(::println)
@@ -10,6 +12,15 @@ fun main() {
     factorialIteration(1).also(::println)
     factorialIteration(2).also(::println)
     factorialIteration(5).also(::println)
+    println()
+    factorialRec(0).also(::println)
+    factorialRec(1).also(::println)
+    factorialRec(2).also(::println)
+    factorialRec(3).also(::println)
+    factorialRec(4).also(::println)
+    factorialRec(5).also(::println)
+    println()
+    factorialWith(5).also(::println)
 }
 
 fun factorialRecursion(n: Int): Int {
@@ -25,4 +36,34 @@ fun factorialIteration(n: Int): Int {
         result *= i
     }
     return result
+}
+
+fun factorialRec(n: Long, accumulator: BigInteger = BigInteger.ONE): BigInteger {
+    val partial = accumulator * BigInteger.valueOf(n)
+    return if (n == 0L) {
+        BigInteger.valueOf(1)
+    } else if (n <= 1) {
+        partial
+    } else {
+        factorialRec(n - 1, partial)
+    }
+}
+
+tailrec fun factorialRec1(n: Long, accumulator: BigInteger = BigInteger.ONE): BigInteger {
+    return if (n == 0L) {
+        BigInteger.valueOf(1)
+    } else if (n <= 1) {
+        accumulator
+    } else {
+        factorialRec(n - 1, accumulator * BigInteger.valueOf(n))
+    }
+}
+
+fun factorialWith(n: Long): BigInteger {
+    return DeepRecursiveFunction<Long, BigInteger> { x ->
+        when {
+            x <= 1 -> BigInteger.ONE
+            else -> x.toBigInteger().multiply(callRecursive(x - 1))
+        }
+    }.invoke(n)
 }
