3dea33a5ac4e642ce7cf958e6b75ebca80ba1ce1
vengateshm <vengateshm.13@gmail.com>
2024-12-24 15:30:43 +0530

    pulse loader animation

---
+++ compose_material3/src/main/java/dev/vengateshm/compose_material3/animations/PulseAnimation.kt
@@ -0,0 +1,60 @@
+package dev.vengateshm.compose_material3.animations
+
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PulseAnimation(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.primary,
+) {
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
+    val progress by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000),
+        ),
+        label = "progress",
+    )
+    Box(
+        modifier = modifier
+            .size(60.dp)
+            .graphicsLayer {
+                scaleX = progress
+                scaleY = progress
+                alpha = 1f - progress
+            }
+            .border(
+                width = 5.dp,
+                color = color,
+                shape = CircleShape,
+            ),
+        contentAlignment = Alignment.Center,
+    ) {
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun PulseAnimationPreview() {
+    MaterialTheme {
+        PulseAnimation(color = Color.Red)
+    }
+}
