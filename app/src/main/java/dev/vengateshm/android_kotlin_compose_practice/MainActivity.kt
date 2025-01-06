9d3db309e231990fd07e19a00e9d1d160fcbba8a
vengateshm <vengateshm.13@gmail.com>
2025-01-06 12:02:26 +0530

    free, paid source set creation

--- app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/MainActivity.kt
+++ app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/MainActivity.kt
@@ -7,20 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.ktx.messaging
-import dev.vengateshm.android_kotlin_compose_practice.animation_with_reading_large_file.AnimationWithFileRead
-import dev.vengateshm.android_kotlin_compose_practice.animations.InfiniteColorTransition
-import dev.vengateshm.android_kotlin_compose_practice.canvas.SVGToXYCoordinatesAnimation
 import dev.vengateshm.android_kotlin_compose_practice.localization_app.LocaleContextWrapper
-import dev.vengateshm.android_kotlin_compose_practice.media3_exoplayer.Media3ExoPlayer
 import dev.vengateshm.android_kotlin_compose_practice.shared_preferences_delegates.stringPrefs
-import dev.vengateshm.android_kotlin_compose_practice.system_bars_hide_show.SystemBarHideShow
-import dev.vengateshm.android_kotlin_compose_practice.text_apis.AutoRefreshTimeTextSample
-import dev.vengateshm.android_kotlin_compose_practice.text_apis.SelectableText
 import dev.vengateshm.android_kotlin_compose_practice.utils.requestPostNotificationsPermission
 
 class MainActivity : ComponentActivity() {
@@ -40,25 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
                 ) {
-//                    EdgeToEdge()
-//                    val context = LocalContext.current
-//                    CompositionLocalProvider(LocalContext provides LocaleContextWrapper(context)) {
-//                        LocalizationApp()
-//                    }
-//                    TermsAcknowledgeText(onPrivacyClicked = { /*TODO*/ }) {
 
-//                    }
-//                    CheeseFactory()
-//                    FormComposable()
-//                    OtpFormField()
-//                    MultiHeaderLazyColumn()
-//                    DraggableShape()
-//                    AutoRefreshTimeTextSample()
-//                    SystemBarHideShow()
-//                    AnimationWithFileRead()
-//                    Media3ExoPlayer()
-//                    SelectableText()
-//                    SVGToXYCoordinatesAnimation()
                 }
             }
         }
---
+++ app/src/paid/kotlin/PaidText.kt
@@ -0,0 +1,8 @@
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PaidText(modifier: Modifier = Modifier) {
+    Text(text = "You are a paid member!")
+}
