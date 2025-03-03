18d9c108213428953d228ae1f02fdfd54963e204
vengateshm <vengateshm.13@gmail.com>
2025-03-03 13:41:24 +0530

    largest unique number using set

--- kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/programs/LargestUniqueNumber.kt
+++ kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/programs/LargestUniqueNumber.kt
@@ -5,6 +5,8 @@ import kotlin.math.max
 fun main() {
     println(largestUniqueNumber(intArrayOf(5, 7, 3, 9, 4, 9, 8, 3, 1)))
     println(largestUniqueNumber(intArrayOf(9, 9, 8, 8)))
+    println(largestUniqueNumberUsingSet(intArrayOf(5, 7, 3, 9, 4, 9, 8, 3, 1)))
+    println(largestUniqueNumberUsingSet(intArrayOf(9, 9, 8, 8)))
 }
 
 fun largestUniqueNumber(arr: IntArray): Int {
@@ -21,6 +23,22 @@ fun largestUniqueNumber(arr: IntArray): Int {
         if (frequencyMap[key] == 1) {
             largestUnique = max(largestUnique, key)
         }
+    }
+    return largestUnique
+}
+
+fun largestUniqueNumberUsingSet(arr: IntArray): Int {
+    val frequencySet = mutableSetOf<Int>()
+    var largestUnique = -1
+    for (num in arr) {
+        if (!frequencySet.contains(num)) {
+            frequencySet.add(num)
+        } else {
+            frequencySet.remove(num)
+        }
+    }
+    for (value in frequencySet) {
+        largestUnique = max(largestUnique, value)
     }
     return largestUnique
 }
