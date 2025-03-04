3568f5c1436ddb1c3484393f7b558f21fdb67835
vengateshm <vengateshm.13@gmail.com>
2025-03-04 22:50:44 +0530

    Create CoroutinesBuildingBlocks.kt

---
+++ kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/coroutines/CoroutinesBuildingBlocks.kt
@@ -0,0 +1,48 @@
+package dev.vengateshm.kotlin_practice.coroutines
+
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+fun CoroutineScope.printInfo() {
+    println("CoroutineScope: $this")
+    println("CoroutineContext: ${this.coroutineContext}")
+    println("Job: ${this.coroutineContext[Job]}")
+    println("")
+    println("")
+}
+
+fun printJobsHierarchy(job: Job, nestLevel: Int = 0) {
+    val indent = "    ".repeat(nestLevel)
+    println("$indent- $job")
+    for (childJob in job.children) {
+        printJobsHierarchy(childJob, nestLevel + 1)
+    }
+}
+
+fun main() {
+    runBlocking {
+        val scopeJob = Job()
+        printJobsHierarchy(scopeJob)
+        val scope = CoroutineScope(scopeJob + CoroutineName("outer scope") + Dispatchers.IO)
+        scope.printInfo()
+        val job = scope.launch(CoroutineName("coroutine") + Dispatchers.Default) {
+            this.printInfo()
+            delay(100)
+            withContext(CoroutineName("withContext") + Dispatchers.IO) {
+                this.printInfo()
+                delay(100)
+                printJobsHierarchy(scopeJob)
+                println("withContext done")
+            }
+            println("coroutine done")
+        }
+        job.join()
+        println("main done")
+    }
+}
