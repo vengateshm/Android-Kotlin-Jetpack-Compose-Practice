03bc16412073efd033a200e288111d1e6c85c164
vengateshm <vengateshm.13@gmail.com>
2025-05-23 15:43:06 +0530

    material3 expressive floating action button menu

--- compose_material3/src/main/java/dev/vengateshm/compose_material3/material3_expressive/Material3ExpressiveSample.kt
+++ compose_material3/src/main/java/dev/vengateshm/compose_material3/material3_expressive/Material3ExpressiveSample.kt
@@ -1,31 +1,53 @@
 package dev.vengateshm.compose_material3.material3_expressive
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.automirrored.filled.Label
+import androidx.compose.material.icons.automirrored.filled.Message
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Archive
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Contacts
+import androidx.compose.material.icons.filled.People
+import androidx.compose.material.icons.filled.Snooze
 import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material3.CircularWavyProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.FloatingActionButtonMenu
+import androidx.compose.material3.FloatingActionButtonMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialShapes
 import androidx.compose.material3.Text
+import androidx.compose.material3.ToggleFloatingActionButton
+import androidx.compose.material3.animateFloatingActionButton
 import androidx.compose.material3.toShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -130,4 +152,88 @@ fun Material3ExpressiveSample(modifier: Modifier = Modifier) {
 @Composable
 fun Material3ExpressiveSamplePreview(modifier: Modifier = Modifier) {
     Material3ExpressiveSample()
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun FABButtonMenuSample(modifier: Modifier = Modifier) {
+    val items =
+        listOf(
+            Icons.AutoMirrored.Filled.Message to "Reply",
+            Icons.Filled.People to "Reply all",
+            Icons.Filled.Contacts to "Forward",
+            Icons.Filled.Snooze to "Snooze",
+            Icons.Filled.Archive to "Archive",
+            Icons.AutoMirrored.Filled.Label to "Label",
+        )
+    Box(modifier = Modifier.fillMaxSize()) {
+        var expanded by rememberSaveable { mutableStateOf(false) }
+        val lazyListState = rememberLazyListState()
+
+        val fabVisible by remember {
+            derivedStateOf {
+                lazyListState.firstVisibleItemIndex == 0
+            }
+        }
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            state = lazyListState,
+        ) {
+            repeat(100) { index ->
+                item {
+                    Text(
+                        text = "List item - $index",
+                        modifier = Modifier
+                            .clickable {}
+                            .fillMaxWidth()
+                            .padding(24.dp),
+                    )
+                }
+            }
+        }
+
+        FloatingActionButtonMenu(
+            modifier = Modifier.align(Alignment.BottomEnd),
+            expanded = expanded,
+            button = {
+                ToggleFloatingActionButton(
+                    checked = expanded,
+                    onCheckedChange = {
+                        expanded = !expanded
+                    },
+                    modifier =
+                        Modifier
+                            .animateFloatingActionButton(
+                                visible = fabVisible || expanded,
+                                alignment = Alignment.BottomEnd,
+                            ),
+                ) {
+                    val imageVector by remember {
+                        derivedStateOf {
+                            if (checkedProgress > 0.5f) Icons.Filled.Close else Icons.Filled.Add
+                        }
+                    }
+                    Icon(
+                        painter = rememberVectorPainter(image = imageVector),
+                        contentDescription = null,
+                    )
+                }
+            },
+        ) {
+            items.forEach { item ->
+                FloatingActionButtonMenuItem(
+                    onClick = { expanded = false },
+                    text = { Text(item.second) },
+                    icon = { Icon(item.first, contentDescription = null) },
+                )
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun FABButtonMenuSamplePreview() {
+    FABButtonMenuSample()
 }
