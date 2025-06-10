d596c10db6aeaa9cae0ab0f639bdf0b80357ece5
vengateshm <vengateshm.13@gmail.com>
2025-06-10 15:47:03 +0530

    triple sum three list

--- kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/programs/PrintPairSum.kt
+++ kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/programs/PrintPairSum.kt
@@ -2,6 +2,14 @@ package dev.vengateshm.kotlin_practice.programs
 
 fun main() {
     println(getSumPairsFromTwoList(listOf(1, 2, 4, 5, 7), listOf(5, 6, 3, 4, 8), 9))
+    println(
+        getSumPairsFromThreeList(
+            listOf(1, 2, 3, 4, 5),
+            listOf(2, 3, 4, 5, 6),
+            listOf(3, 4, 5, 6, 7),
+            9,
+        ),
+    )
 }
 
 fun getSumPairsFromTwoList(nums1: List<Int>, nums2: List<Int>, target: Int): List<Pair<Int, Int>> {
@@ -11,6 +19,27 @@ fun getSumPairsFromTwoList(nums1: List<Int>, nums2: List<Int>, target: Int): Lis
         val complement = target - num
         if (set.contains(complement)) {
             result.add(Pair(complement, num))
+        }
+    }
+    return result
+}
+
+fun getSumPairsFromThreeList(
+    nums1: List<Int>,
+    nums2: List<Int>,
+    nums3: List<Int>,
+    target: Int,
+): List<Triple<Int, Int, Int>> {
+    val set1 = nums1.toHashSet()
+    val set2 = nums2.toHashSet()
+    val result = mutableListOf<Triple<Int, Int, Int>>()
+    for (num3 in nums3) {
+        val remainingSum = target - num3
+        for (num1 in set1) {
+            val complement = remainingSum - num1
+            if (set2.contains(complement)) {
+                result.add(Triple(num1, complement, num3))
+            }
         }
     }
     return result
