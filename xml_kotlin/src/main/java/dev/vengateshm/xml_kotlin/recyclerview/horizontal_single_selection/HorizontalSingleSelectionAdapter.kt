979497c9194e23aebfe37ec20ffd2c311600dea1
vengateshm <vengateshm.13@gmail.com>
2025-06-23 20:24:05 +0530

    single selection list item linear interpolation

--- xml_kotlin/src/main/java/dev/vengateshm/xml_kotlin/recyclerview/horizontal_single_selection/HorizontalSingleSelectionAdapter.kt
+++ xml_kotlin/src/main/java/dev/vengateshm/xml_kotlin/recyclerview/horizontal_single_selection/HorizontalSingleSelectionAdapter.kt
@@ -1,97 +1,117 @@
 package dev.vengateshm.xml_kotlin.recyclerview.horizontal_single_selection
 
+import android.animation.AnimatorSet
 import android.animation.ValueAnimator
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
 import android.widget.TextView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import dev.vengateshm.xml_kotlin.databinding.HorizontalSingleSelectionItemBinding
 
 class HorizontalSingleSelectionAdapter(
-    private val onItemClicked: (Item) -> Unit,
+  private val onItemClicked: (Item) -> Unit,
 ) : ListAdapter<Item, HorizontalSingleSelectionAdapter.ItemViewHolder>(ItemDiffCallback()) {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int,
-    ): ItemViewHolder {
-        val binding = HorizontalSingleSelectionItemBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent,
-            false,
-        )
-        return ItemViewHolder(binding, onItemClicked)
+  override fun onCreateViewHolder(
+    parent: ViewGroup,
+    viewType: Int,
+  ): ItemViewHolder {
+    val binding = HorizontalSingleSelectionItemBinding.inflate(
+      LayoutInflater.from(parent.context),
+      parent,
+      false,
+    )
+    return ItemViewHolder(binding, onItemClicked)
+  }
+
+  override fun onBindViewHolder(
+    holder: ItemViewHolder,
+    position: Int,
+  ) {
+    holder.bind(getItem(position))
+  }
+
+  class ItemViewHolder(
+    private val binding: HorizontalSingleSelectionItemBinding,
+    private val onItemClicked: (Item) -> Unit,
+  ) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: Item) {
+      binding.root.setOnClickListener {
+        onItemClicked(item)
+      }
+      binding.textView.text = item.name
+      animateItem(binding, item.isSelected)
+      rotateText(binding.textView, item.isSelected)
     }
 
-    override fun onBindViewHolder(
-        holder: ItemViewHolder,
-        position: Int,
+    private fun animateItem(
+      binding: HorizontalSingleSelectionItemBinding,
+      isSelected: Boolean,
     ) {
-        holder.bind(getItem(position))
+      val targetHeight = if (isSelected) 200 else 175
+      val targetWidth = if (isSelected) 200 else 50
+
+      val view = binding.itemRoot
+//            view.pivotY = view.height.toFloat()
+
+      val shouldAnimate = true
+      if (shouldAnimate) {
+        val heightAnimator =
+          ValueAnimator.ofInt(view.height, dpToPx(view.context, targetHeight))
+        val widthAnimator =
+          ValueAnimator.ofInt(view.width, dpToPx(view.context, targetWidth))
+
+        heightAnimator
+          .apply {
+            duration = 300
+            interpolator = LinearInterpolator()
+          }
+          .addUpdateListener {
+            val value = it.animatedValue as Int
+            view.layoutParams.height = value
+            view.requestLayout()
+          }
+        widthAnimator
+          .apply {
+            duration = 300
+            interpolator = LinearInterpolator()
+          }
+          .addUpdateListener {
+            val value = it.animatedValue as Int
+            view.layoutParams.width = value
+            view.requestLayout()
+          }
+        val playSequentially = false
+        if (playSequentially) {
+          heightAnimator.start()
+          widthAnimator.start()
+        } else {
+          AnimatorSet().apply {
+            playTogether(widthAnimator, heightAnimator)
+            interpolator = LinearInterpolator()
+            duration = 200
+            start()
+          }
+        }
+      } else {
+        view.layoutParams.width = dpToPx(view.context, targetWidth)
+        view.layoutParams.height = dpToPx(view.context, targetHeight)
+        view.requestLayout()
+      }
     }
 
-    class ItemViewHolder(
-        private val binding: HorizontalSingleSelectionItemBinding,
-        private val onItemClicked: (Item) -> Unit,
-    ) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(item: Item) {
-            binding.root.setOnClickListener {
-                onItemClicked(item)
-            }
-            binding.textView.text = item.name
-            animateItem(binding, item.isSelected)
-            rotateText(binding.textView, item.isSelected)
-        }
-
-        private fun animateItem(
-            binding: HorizontalSingleSelectionItemBinding,
-            isSelected: Boolean,
-        ) {
-            val targetHeight = if (isSelected) 200 else 175
-            val targetWidth = if (isSelected) 200 else 50
-
-            val view = binding.itemRoot
-            view.pivotY = view.height.toFloat()
-
-            if (false) {
-                val heightAnimator =
-                    ValueAnimator.ofInt(view.height, dpToPx(view.context, targetHeight))
-                val widthAnimator =
-                    ValueAnimator.ofInt(view.width, dpToPx(view.context, targetWidth))
-
-                heightAnimator.addUpdateListener {
-                    val value = it.animatedValue as Int
-                    view.layoutParams.height = value
-                    view.requestLayout()
-                }
-                widthAnimator.addUpdateListener {
-                    val value = it.animatedValue as Int
-                    view.layoutParams.width = value
-                    view.requestLayout()
-                }
-
-                heightAnimator.duration = 300
-                widthAnimator.duration = 300
-                heightAnimator.start()
-                widthAnimator.start()
-            } else {
-                view.layoutParams.width = dpToPx(view.context, targetWidth)
-                view.layoutParams.height = dpToPx(view.context, targetHeight)
-                view.requestLayout()
-            }
-        }
-
-        private fun rotateText(textView: TextView, isSelected: Boolean) {
-            val rotation = if (isSelected) 0f else 270f
+    private fun rotateText(textView: TextView, isSelected: Boolean) {
+      val rotation = if (isSelected) 0f else 270f
 //            textView.animate().rotation(rotation).setDuration(300).start()
-            textView.rotation = rotation
-        }
-
-        private fun dpToPx(context: Context, dp: Int): Int {
-            return (dp * context.resources.displayMetrics.density).toInt()
-        }
+      textView.rotation = rotation
     }
+
+    private fun dpToPx(context: Context, dp: Int): Int {
+      return (dp * context.resources.displayMetrics.density).toInt()
+    }
+  }
 }
--- xml_kotlin/src/main/java/dev/vengateshm/xml_kotlin/recyclerview/horizontal_single_selection/HorizontalSingleSelectionScrollViewFragment.kt
+++ xml_kotlin/src/main/java/dev/vengateshm/xml_kotlin/recyclerview/horizontal_single_selection/HorizontalSingleSelectionScrollViewFragment.kt
@@ -1,10 +1,13 @@
 package dev.vengateshm.xml_kotlin.recyclerview.horizontal_single_selection
 
+import android.animation.AnimatorSet
 import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.BounceInterpolator
+import android.view.animation.LinearInterpolator
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -14,85 +17,102 @@ import dev.vengateshm.xml_kotlin.R
 
 class HorizontalSingleSelectionScrollViewFragment : Fragment() {
 
-    private val viewModel: HorizontalSingleSelectionViewModel by viewModels()
-    private lateinit var itemContainer: LinearLayout
-    private val itemViews = mutableListOf<View>()
+  private val viewModel: HorizontalSingleSelectionViewModel by viewModels()
+  private lateinit var itemContainer: LinearLayout
+  private val itemViews = mutableListOf<View>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(
-            R.layout.fragment_horizontal_single_selection_list_scroll_view,
-            container,
-            false,
-        )
-    }
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?,
+  ): View? {
+    return inflater.inflate(
+      R.layout.fragment_horizontal_single_selection_list_scroll_view,
+      container,
+      false,
+    )
+  }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
-        itemContainer = view.findViewById(R.id.itemContainer)
-        viewModel.items.observe(viewLifecycleOwner) { items ->
-            itemContainer.removeAllViews()
-            itemViews.clear()
-            items.forEachIndexed { index, item ->
-                val view = LayoutInflater.from(context)
-                    .inflate(R.layout.horizontal_single_selection_item, itemContainer, false)
-                val textView = view.findViewById<TextView>(R.id.textView)
-                val itemRoot = view.findViewById<FrameLayout>(R.id.itemRoot)
-                textView.text = item.name
+    itemContainer = view.findViewById(R.id.itemContainer)
+    viewModel.items.observe(viewLifecycleOwner) { items ->
+      itemContainer.removeAllViews()
+      itemViews.clear()
+      items.forEachIndexed { index, item ->
+        val view = LayoutInflater.from(context)
+          .inflate(R.layout.horizontal_single_selection_item, itemContainer, false)
+        val textView = view.findViewById<TextView>(R.id.textView)
+        val itemRoot = view.findViewById<FrameLayout>(R.id.itemRoot)
+        textView.text = item.name
 
-                updateView(itemRoot, textView, item.isSelected)
+        updateView(itemRoot, textView, item.isSelected)
 
-                view.setOnClickListener {
-                    viewModel.onItemClicked(item)
-                }
-                itemContainer.addView(view)
-                itemViews.add(view)
-            }
+        view.setOnClickListener {
+          viewModel.onItemClicked(item)
         }
+        itemContainer.addView(view)
+        itemViews.add(view)
+      }
     }
+  }
 
-    private fun updateView(view: View, textView: TextView, isSelected: Boolean) {
-        val targetHeight = dpToPx(if (isSelected) 200 else 175)
-        val targetWidth = dpToPx(if (isSelected) 200 else 50)
+  private fun updateView(view: View, textView: TextView, isSelected: Boolean) {
+    val targetHeight = dpToPx(if (isSelected) 200 else 175)
+    val targetWidth = dpToPx(if (isSelected) 200 else 50)
 
-        animateDimension(view, targetWidth, targetHeight)
+    animateDimension(view, targetWidth, targetHeight)
 
-        val targetRotation = if (isSelected) 0f else 270f
-        //textView.animate().rotation(targetRotation).setDuration(300).start()
-        textView.rotation = targetRotation
-    }
+    val targetRotation = if (isSelected) 0f else 270f
+    //textView.animate().rotation(targetRotation).setDuration(300).start()
+    textView.rotation = targetRotation
+  }
 
-    private fun animateDimension(view: View, targetWidth: Int, targetHeight: Int) {
-        if (false) {
-            val heightAnimator = ValueAnimator.ofInt(view.height, targetHeight)
-            val widthAnimator = ValueAnimator.ofInt(view.width, targetWidth)
+  private fun animateDimension(view: View, targetWidth: Int, targetHeight: Int) {
+    val shouldAnimate = true
+    if (shouldAnimate) {
+      val heightAnimator = ValueAnimator.ofInt(view.height, targetHeight)
+      val widthAnimator = ValueAnimator.ofInt(view.width, targetWidth)
 
-            heightAnimator.addUpdateListener {
-                view.layoutParams.height = it.animatedValue as Int
-                view.requestLayout()
-            }
+      heightAnimator.addUpdateListener {
+        view.layoutParams.height = it.animatedValue as Int
+        view.requestLayout()
+      }
 
-            widthAnimator.addUpdateListener {
-                view.layoutParams.width = it.animatedValue as Int
-                view.requestLayout()
-            }
+      widthAnimator.addUpdateListener {
+        view.layoutParams.width = it.animatedValue as Int
+        view.requestLayout()
+      }
 
-            heightAnimator.duration = 300
-            widthAnimator.duration = 300
-            heightAnimator.start()
-            widthAnimator.start()
-        } else {
-            view.layoutParams.width = targetWidth
-            view.layoutParams.height = targetHeight
-            view.requestLayout()
+      heightAnimator.apply {
+        duration = 300
+        interpolator = LinearInterpolator()
+      }
+      widthAnimator.apply {
+        duration = 300
+        interpolator = LinearInterpolator()
+      }
+      val playSequentially = false
+      if (playSequentially) {
+        heightAnimator.start()
+        widthAnimator.start()
+      } else {
+        AnimatorSet().apply {
+          playTogether(widthAnimator, heightAnimator)
+          interpolator = BounceInterpolator()
+          duration = 200
+          start()
         }
+      }
+    } else {
+      view.layoutParams.width = targetWidth
+      view.layoutParams.height = targetHeight
+      view.requestLayout()
     }
+  }
 
-    private fun dpToPx(dp: Int): Int {
-        return (dp * resources.displayMetrics.density).toInt()
-    }
+  private fun dpToPx(dp: Int): Int {
+    return (dp * resources.displayMetrics.density).toInt()
+  }
 }
