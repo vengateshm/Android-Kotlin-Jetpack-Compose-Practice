ed05fb9fd3bb2da00f1cff1c2a8a48e7b26d7cc4
vengateshm <vengateshm.13@gmail.com>
2025-08-07 16:21:09 +0530

    dependencies version update

--- app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/insta_reels_ui/composables/ReelsView.kt
+++ app/src/main/java/dev/vengateshm/android_kotlin_compose_practice/insta_reels_ui/composables/ReelsView.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.skydoves.landscapist.ImageOptions
 import com.skydoves.landscapist.glide.GlideImage
 import dev.vengateshm.android_kotlin_compose_practice.R
 import dev.vengateshm.android_kotlin_compose_practice.insta_reels_ui.mockData.MockData
@@ -44,235 +45,243 @@ val horizontalPadding = 12.dp
 
 @Composable
 fun ReelsView() {
-    Box(
-        modifier =
-            Modifier
-                .background(color = Color.Black),
-    ) {
-        ReelsList()
-        ReelsHeader()
-    }
+  Box(
+    modifier =
+      Modifier
+        .background(color = Color.Black),
+  ) {
+    ReelsList()
+    ReelsHeader()
+  }
 }
 
 @Composable
 fun ReelsList() {
-    val reelList = MockData.getReelList()
-    LazyColumn {
-        items(reelList.size) { index ->
-            Box(
-                modifier =
-                    Modifier
-                        .fillParentMaxSize(),
-            ) {
-                // Bottom controls
-                Column(
-                    modifier =
-                        Modifier
-                            .align(Alignment.BottomStart),
-                ) {
-                    ReelFooter(reelList[index])
-                    Divider()
-                }
-            }
+  val reelList = MockData.getReelList()
+  LazyColumn {
+    items(reelList.size) { index ->
+      Box(
+        modifier =
+          Modifier
+            .fillParentMaxSize(),
+      ) {
+        // Bottom controls
+        Column(
+          modifier =
+            Modifier
+              .align(Alignment.BottomStart),
+        ) {
+          ReelFooter(reelList[index])
+          Divider()
         }
+      }
     }
+  }
 }
 
 @Composable
 fun ReelFooter(reel: Reel) {
-    Row(
-        modifier =
-            Modifier
-                .fillMaxWidth()
-                .padding(
-                    start = 18.dp,
-                    bottom = 18.dp,
-                ),
-        verticalAlignment = Alignment.Bottom,
-    ) {
-        FooterUserData(
-            reel = reel,
-            modifier = Modifier.weight(8f),
-        )
+  Row(
+    modifier =
+      Modifier
+        .fillMaxWidth()
+        .padding(
+          start = 18.dp,
+          bottom = 18.dp,
+        ),
+    verticalAlignment = Alignment.Bottom,
+  ) {
+    FooterUserData(
+      reel = reel,
+      modifier = Modifier.weight(8f),
+    )
 
-        FooterUserAction(
-            reel = reel,
-            modifier = Modifier.weight(2f),
-        )
-    }
+    FooterUserAction(
+      reel = reel,
+      modifier = Modifier.weight(2f),
+    )
+  }
 }
 
 @Composable
 fun FooterUserData(
-    reel: Reel,
-    modifier: Modifier,
+  reel: Reel,
+  modifier: Modifier,
 ) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.Center,
-    ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            GlideImage(
-                imageModel = reel.userImage,
-                contentScale = ContentScale.Crop,
-                modifier =
-                    Modifier
-                        .size(28.dp)
-                        .background(color = Color.Gray, shape = CircleShape)
-                        .clip(CircleShape),
-                contentDescription = null,
-            )
+  Column(
+    modifier = modifier,
+    verticalArrangement = Arrangement.Center,
+  ) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+      GlideImage(
+        imageModel = { reel.userImage },
+        imageOptions = ImageOptions(
+          contentDescription = null,
+          contentScale = ContentScale.Crop,
+        ),
+        modifier =
+          Modifier
+            .size(28.dp)
+            .background(color = Color.Gray, shape = CircleShape)
+            .clip(CircleShape),
+      )
 
-            Spacer(modifier = Modifier.width(horizontalPadding))
+      Spacer(modifier = Modifier.width(horizontalPadding))
 
-            Text(
-                text = reel.userName,
-                color = Color.White,
-                style = MaterialTheme.typography.subtitle2,
-            )
+      Text(
+        text = reel.userName,
+        color = Color.White,
+        style = MaterialTheme.typography.subtitle2,
+      )
 
-            Spacer(modifier = Modifier.width(horizontalPadding))
+      Spacer(modifier = Modifier.width(horizontalPadding))
 //            Canvas(modifier = Modifier.size(5.dp), onDraw = {
 //                drawCircle(
 //                    color = Color.White,
 //                    radius = 8f
 //                )
 //            })
-            Spacer(modifier = Modifier.width(horizontalPadding))
-            Text(
-                text = "Follow",
-                color = Color.White,
-                style = MaterialTheme.typography.subtitle2,
-                modifier =
-                    Modifier
-                        .border(
-                            1.dp,
-                            shape = RoundedCornerShape(2.dp),
-                            color = Color.White,
-                        )
-                        .padding(
-                            horizontal = 8.dp,
-                            vertical = 4.dp,
-                        ),
+      Spacer(modifier = Modifier.width(horizontalPadding))
+      Text(
+        text = "Follow",
+        color = Color.White,
+        style = MaterialTheme.typography.subtitle2,
+        modifier =
+          Modifier
+            .border(
+              1.dp,
+              shape = RoundedCornerShape(2.dp),
+              color = Color.White,
             )
-        }
-
-        Spacer(modifier = Modifier.height(horizontalPadding))
-        Text(text = reel.comment, color = Color.White)
-        Spacer(modifier = Modifier.height(horizontalPadding))
-
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Text(reel.userName, color = Color.White)
-            Spacer(modifier = Modifier.width(horizontalPadding))
-            Canvas(modifier = Modifier.size(5.dp), onDraw = {
-                drawCircle(
-                    color = Color.White,
-                    radius = 2f,
-                )
-            })
-            Spacer(modifier = Modifier.width(horizontalPadding))
-            Text(
-                text = "Original Audio",
-                color = Color.White,
-            )
-        }
+            .padding(
+              horizontal = 8.dp,
+              vertical = 4.dp,
+            ),
+      )
     }
+
+    Spacer(modifier = Modifier.height(horizontalPadding))
+    Text(text = reel.comment, color = Color.White)
+    Spacer(modifier = Modifier.height(horizontalPadding))
+
+    Row(
+      verticalAlignment = Alignment.CenterVertically,
+    ) {
+      Text(reel.userName, color = Color.White)
+      Spacer(modifier = Modifier.width(horizontalPadding))
+      Canvas(
+        modifier = Modifier.size(5.dp),
+        onDraw = {
+          drawCircle(
+            color = Color.White,
+            radius = 2f,
+          )
+        },
+      )
+      Spacer(modifier = Modifier.width(horizontalPadding))
+      Text(
+        text = "Original Audio",
+        color = Color.White,
+      )
+    }
+  }
 }
 
 @Composable
 fun FooterUserAction(
-    reel: Reel,
-    modifier: Modifier,
+  reel: Reel,
+  modifier: Modifier,
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier,
-    ) {
-        UserActionWithText(
-            drawableRes = R.drawable.ic_outlined_favorite,
-            label = reel.likesCount.toString(),
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-        UserActionWithText(
-            drawableRes = R.drawable.ic_outlined_comment,
-            label = reel.commentsCount.toString(),
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-        UserAction(drawableRes = R.drawable.ic_dm)
-        Spacer(modifier = Modifier.height(24.dp))
-        Icon(imageVector = Icons.Default.MoreVert, tint = Color.White, contentDescription = null)
-        Spacer(modifier = Modifier.height(24.dp))
-        GlideImage(
-            imageModel = reel.userImage,
-            modifier =
-                Modifier
-                    .size(28.dp)
-                    .background(color = Color.Gray, shape = RoundedCornerShape(6.dp))
-                    .clip(RoundedCornerShape(6.dp)),
-            contentDescription = null,
-        )
-    }
+  Column(
+    horizontalAlignment = Alignment.CenterHorizontally,
+    modifier = modifier,
+  ) {
+    UserActionWithText(
+      drawableRes = R.drawable.ic_outlined_favorite,
+      label = reel.likesCount.toString(),
+    )
+    Spacer(modifier = Modifier.height(24.dp))
+    UserActionWithText(
+      drawableRes = R.drawable.ic_outlined_comment,
+      label = reel.commentsCount.toString(),
+    )
+    Spacer(modifier = Modifier.height(24.dp))
+    UserAction(drawableRes = R.drawable.ic_dm)
+    Spacer(modifier = Modifier.height(24.dp))
+    Icon(imageVector = Icons.Default.MoreVert, tint = Color.White, contentDescription = null)
+    Spacer(modifier = Modifier.height(24.dp))
+    GlideImage(
+      imageModel = { reel.userImage },
+      imageOptions = ImageOptions(
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+      ),
+      modifier =
+        Modifier
+          .size(28.dp)
+          .background(color = Color.Gray, shape = RoundedCornerShape(6.dp))
+          .clip(RoundedCornerShape(6.dp)),
+    )
+  }
 }
 
 @Composable
 fun UserAction(
-    @DrawableRes drawableRes: Int,
+  @DrawableRes drawableRes: Int,
 ) {
-    Icon(
-        bitmap = ImageBitmap.imageResource(id = drawableRes),
-        tint = Color.White,
-        modifier = Modifier.size(16.dp),
-        contentDescription = null,
-    )
+  Icon(
+    bitmap = ImageBitmap.imageResource(id = drawableRes),
+    tint = Color.White,
+    modifier = Modifier.size(16.dp),
+    contentDescription = null,
+  )
 }
 
 @Composable
 fun UserActionWithText(
-    @DrawableRes drawableRes: Int,
-    label: String,
+  @DrawableRes drawableRes: Int,
+  label: String,
 ) {
-    Icon(
-        bitmap = ImageBitmap.imageResource(id = drawableRes),
-        tint = Color.White,
-        modifier = Modifier.size(28.dp),
-        contentDescription = null,
-    )
-    Spacer(modifier = Modifier.height(6.dp))
-    Text(
-        text = label,
-        color = Color.White,
-        fontSize = 13.sp,
-        fontWeight = FontWeight.SemiBold,
-    )
+  Icon(
+    bitmap = ImageBitmap.imageResource(id = drawableRes),
+    tint = Color.White,
+    modifier = Modifier.size(28.dp),
+    contentDescription = null,
+  )
+  Spacer(modifier = Modifier.height(6.dp))
+  Text(
+    text = label,
+    color = Color.White,
+    fontSize = 13.sp,
+    fontWeight = FontWeight.SemiBold,
+  )
 }
 
 @Composable
 fun ReelsHeader() {
-    Row(
-        modifier =
-            Modifier
-                .fillMaxWidth()
-                .padding(
-                    horizontal = horizontalPadding,
-                    vertical = verticalPadding,
-                ),
-        horizontalArrangement = Arrangement.SpaceBetween,
-    ) {
-        Text(
-            text = "Reels",
-            color = Color.White,
-            fontWeight = FontWeight.SemiBold,
-            fontSize = 21.sp,
-        )
-        Icon(
-            bitmap = ImageBitmap.imageResource(id = R.drawable.ic_outlined_camera),
-            tint = Color.White,
-            modifier =
-                Modifier
-                    .size(24.dp),
-            contentDescription = "Camera Icon",
-        )
-    }
+  Row(
+    modifier =
+      Modifier
+        .fillMaxWidth()
+        .padding(
+          horizontal = horizontalPadding,
+          vertical = verticalPadding,
+        ),
+    horizontalArrangement = Arrangement.SpaceBetween,
+  ) {
+    Text(
+      text = "Reels",
+      color = Color.White,
+      fontWeight = FontWeight.SemiBold,
+      fontSize = 21.sp,
+    )
+    Icon(
+      bitmap = ImageBitmap.imageResource(id = R.drawable.ic_outlined_camera),
+      tint = Color.White,
+      modifier =
+        Modifier
+          .size(24.dp),
+      contentDescription = "Camera Icon",
+    )
+  }
 }
