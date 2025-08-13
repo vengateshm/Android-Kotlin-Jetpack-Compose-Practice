8fde7bca09f67172b54671ff983e4f2e457de020
vengateshm <vengateshm.13@gmail.com>
2025-08-13 15:25:58 +0530

    sealed interface context sensitive resolution

--- kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/context_sensitive_resolution/ContextSensitiveResolution.kt
+++ kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/context_sensitive_resolution/ContextSensitiveResolution.kt
@@ -1,5 +1,10 @@
 package dev.vengateshm.kotlin_practice.context_sensitive_resolution
 
+import dev.vengateshm.kotlin_practice.context_sensitive_resolution.Color.Companion.BLACK
+import dev.vengateshm.kotlin_practice.context_sensitive_resolution.Color.Companion.KODEE
+import dev.vengateshm.kotlin_practice.context_sensitive_resolution.Color.Companion.WHITE
+import java.math.BigInteger
+
 enum class NetworkError {
   Connection,
   Authentication,
@@ -32,4 +37,38 @@ fun main() {
   println(message(error = NetworkError.Connection))
   println(parseError(errorCode = 2))
   log(error = NetworkError.Authentication)
+}
+
+sealed interface NetworkError1 {
+  data class Connection(val responseCode: Int) : NetworkError1
+  data class Authentication(val message: String) : NetworkError1
+  data object Permission : NetworkError1
+  data object Unknown : NetworkError1
+}
+
+class Color(r: Int, g: Int, b: Int) {
+  companion object {
+    val WHITE: Color = Color(255, 255, 255)
+    val BLACK: Color = Color(0, 0, 0)
+    val KODEE: Color = Color(0, 0, 0)
+    fun background(): Color = Color(0, 0, 0)
+  }
+}
+
+//val Color.Companion.KODEE get() = Color(127, 82, 255)
+
+fun testColor(color: Color) {
+  when (color) {
+    WHITE -> println("White")
+    BLACK -> println("White")
+    KODEE -> println("White")
+    else -> println("Other")
+  }
+}
+
+fun withJavaStatistics(bigInteger: BigInteger) {
+  when (bigInteger) {
+    BigInteger.ZERO, BigInteger.ONE, BigInteger.TWO, BigInteger.TEN -> println("Known big integer")
+    else -> println("Unknown big integer")
+  }
 }
