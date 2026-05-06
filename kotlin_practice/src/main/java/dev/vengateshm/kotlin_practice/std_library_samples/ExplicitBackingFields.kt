599c144e2455f86bf6555114d3aebcaca464fad3
vengateshm <vengateshm.13@gmail.com>
2026-05-07 02:27:58 +0530

    ExplicitBackingFields

--- kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/std_library_samples/ExplicitBackingFields.kt
+++ kotlin_practice/src/main/java/dev/vengateshm/kotlin_practice/std_library_samples/ExplicitBackingFields.kt
@@ -7,35 +7,45 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
-    runBlocking {
-        val searchScreen = SearchScreen()
-        launch {
-            searchScreen.uiState.collect {
-                println(it)
-            }
-        }
-        launch {
-            searchScreen.search()
-        }
+  runBlocking {
+    val searchScreen = SearchScreen()
+    launch {
+      searchScreen.uiState.collect {
+        println(it)
+      }
     }
+    launch {
+      searchScreen.search()
+    }
+  }
 }
 
 class SearchScreen {
-    val uiState: StateFlow<UiStatus>
-        field = MutableStateFlow<UiStatus>(UiStatus.None)
+  val uiState: StateFlow<UiStatus>
+    field = MutableStateFlow<UiStatus>(UiStatus.None)
 
-    suspend fun search() {
-        uiState.value = UiStatus.None
-        delay(1000L)
-        uiState.value = UiStatus.Loading
-        delay(1000L)
-        uiState.value = UiStatus.Success
-    }
+  val dogs: List<String> field : MutableList<String> = mutableListOf<String>()
+  val dogs1: List<String>
+    field = mutableListOf<String>()
+
+  suspend fun search() {
+    uiState.value = UiStatus.None
+    delay(1000L)
+    uiState.value = UiStatus.Loading
+    delay(1000L)
+    uiState.value = UiStatus.Success
+
+    dogs.add("Dog")
+    dogs1.add("Dog1")
+
+    println(dogs)
+    println(dogs1)
+  }
 }
 
 sealed interface UiStatus {
-    data object None : UiStatus
-    data object Loading : UiStatus
-    data object Success : UiStatus
-    data object Error : UiStatus
+  data object None : UiStatus
+  data object Loading : UiStatus
+  data object Success : UiStatus
+  data object Error : UiStatus
 }
